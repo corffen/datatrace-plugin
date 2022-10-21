074ca41521d97c36bdf103d77c382f7acf02b57e
GitHub Action <action@github.com>
2022-10-21 13:31:57 +0000

    Template cleanup

--- src/main/kotlin/com/github/corffen/datatraceplugin/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/corffen/datatraceplugin/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.corffen.datatraceplugin.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.corffen.datatraceplugin.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/corffen/datatraceplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/corffen/datatraceplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.corffen.datatraceplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.corffen.datatraceplugin.MyBundle
 
 class MyApplicationService {
 
